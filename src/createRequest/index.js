35a1373d77fe8f8cb269ae7c4b6516954353338b
Whien <sal95610@gmail.com>
2018-05-22 19:29:00 +0800

    change: aws
    * move heroku to aws

--- src/createRequest/index.js
+++ src/createRequest/index.js
@@ -4,8 +4,8 @@ const createRequest = ({ apiKey, mode }) =>
   axios.create({
     baseURL:
       mode === 'test'
-        ? 'https://yosgo-api-policer-dev.herokuapp.com/_api/v1/'
-        : 'https://yosgo-api-policer.herokuapp.com/_api/v1/',
+        ? 'https://awsapi.yosgo.com/_api/v1/'
+        : 'https://awsapi.yosgo.com/_api/v1/',
     timeout: 20000,
     headers: {
       'content-type': 'application/json',
